1ab2bd0a902e632386cea0a4f42a81282b81b470
Kirill Kovalevski <kirill.kovalevski@mail.huji.ac.il>
2025-09-27 06:17:56 +0300

    Setup GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Auto-sets correct base path on GitHub Pages (e.g. /your-repo-name/)
 export default defineConfig({
   plugins: [react()],
-  base: process.env.BASE_PATH || "/",
-  build: { sourcemap: true },
+  // IMPORTANT: repo name = "Converter"
+  base: "/Converter/",
 });
